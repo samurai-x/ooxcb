cfc7e6b6bdd5e9cc9276fb50d4b06bb13775033f
Friedrich Weber <fred@reichbier.de>
2010-09-27 17:11:37 +0200

    Teh fuck.

--- ooxcb/contrib/pil.py
+++ ooxcb/contrib/pil.py
@@ -80,11 +80,11 @@ def get_pil_image(drawable, x=0, y=0, width=None, height=None):
     reply = drawable.get_image(
            ImageFormat.ZPixmap,
            x, y,
-           (width, height),
+           width, height,
            0xffffffff # to get all bits (this should be big enough)
            ).reply()
     data = ''.join(map(chr, reply.data))
-    return Image.fromstring("RGBX", width, height,
+    return Image.fromstring("RGBX", (width, height),
             data, "raw", "BGRX").convert("RGB")
 
 def mixin():
